2ca707e9e7d16dc981fbc09da2f5d75425e8d93b
Antonio2193 <161360723+Antonio2193@users.noreply.github.com>
2024-09-18 19:09:48 +0200

    aggiunti controllers

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,107 @@
+import Post from "../models/postSchema.js";
+import transport from "../services/mailService.js";
+import User from "../models/userSchema.js";
+
+
+export const getPosts = async (req, res) => {
+    const page = req.query.page || 1;
+    let perPage = req.query.perPage || 8;
+    perPage = perPage > 10 ? 9 : perPage;
+
+    try {
+        const posts = await Post.find(req.query.title ? { title: { $regex: req.query.title, $options: "i" } } : {})
+            .collation({ locale: 'it' }) // Ignora maiuscole e minuscole nell'ordinamento
+            .sort({ title: 1, category: 1 })
+            .skip((page - 1) * perPage)
+            .limit(perPage)
+            .populate('user', 'name avatar') // Popola i campi 'name' e 'avatar' dell'utente
+            .exec();
+
+        const totalResults = await Post.countDocuments(req.query.title ? { title: { $regex: req.query.title, $options: "i" } } : {});
+        const totalPages = Math.ceil(totalResults / perPage);
+
+        res.send({
+            dati: posts,
+            page,
+            totalPages,
+            totalResults,
+        });
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        res.status(404).send({ message: "Posts not found" });
+    }
+};
+
+
+export const createPost = async (req, res) => {
+    const post = new Post({...req.body, cover: req.file.path, readTime:JSON.parse(req.body.readTime)});
+    let newPost 
+    try {
+        newPost = await post.save();
+        res.send(newPost)
+    } catch (error) {
+        return res.status(400).send({ message: error.message });
+    }
+
+    try {
+        const user = await User.findById(newPost.user);
+        await transport.sendMail({
+            from: 'noreply@joker.com', // sender address
+            to: user.email, // list of receivers
+            subject: "New Post", // Subject line
+            text: "You have created a new post", // plain text body
+            html: "<b>You have created a new post</b>", // html body
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getSinglePost = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const post = await Post.findById(id).populate('user');
+        res.status(200).send(post);
+    } catch (error) {
+        res.status(404).send({ message: "Post not found" });
+    }   
+}
+
+
+export const editPost = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, req.body , {new: true});
+        post.cover = post.cover ? post.cover : "https://picsum.photos/200/300";
+        await post.save();
+
+        res.status(200).send(post);
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    try{
+        if (await Post.exists({_id: id})){
+            await Post.findByIdAndDelete(id);
+            res.status(200).send({message: `Post ${id} has been deleted`});
+        }else{
+            res.status(400).send({ message: `Post ${id} not found` });
+        }
+    } catch (error) {
+        res.status(400).send({ message: `Post ${id} not found` });
+    }
+}
+
+export const patchPost = async (req, res) => {
+    const { blogPostId } = req.params;
+    try {
+        const post = await Post.findByIdAndUpdate(blogPostId, {cover: req.file.path}, {new: true});
+        await post.save();
+        res.status(200).send(post);
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+}
---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,83 @@
+import User from "../models/userSchema.js";
+
+export const getUsers = async (req, res) => {
+    const page = req.query.page || 1
+    let perPage = req.query.perPage || 8
+    perPage = perPage > 10 ? 8 : perPage
+    try {
+        const users = await User.find(req.query.name ? {name: {$regex: req.query.name, $options: "i"}}:{})
+        .collation({locale: 'it'}) //serve per ignorare maiuscole e minuscole nell'ordine alfabetico del sort
+        .sort({ name:1, surname:1})
+        .skip((page-1)*perPage)
+        .limit(perPage);
+        const totalResults = await User.countDocuments()// mi da il numero totale di documenti
+        const totalPages = Math.ceil(totalResults / perPage )
+        res.send({
+            dati: users,
+            page,
+            totalPages,
+            totalResults,
+        })
+    } catch (error) {
+        res.status(404).send({ message: "Users not found" });
+    } 
+}
+
+export const createUser = async (req, res) => {
+    const user = new User(req.body);
+    user.avatar = user.avatar ? user.avatar : "https://picsum.photos/40";
+    try {
+        const newUser = await user.save();
+        res.status(200).send(newUser);
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+}
+
+export const getSingleUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(404).send({ message: "User not found" });
+    }   
+}
+
+
+export const editUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findByIdAndUpdate(id, req.body , {new: true});
+        user.avatar = user.avatar ? user.avatar : "https://picsum.photos/40";
+        await user.save();
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    try{
+        if (await User.exists({_id: id})){
+            await User.findByIdAndDelete(id);
+            res.status(200).send({message: `User ${id} has been deleted`});
+        }else{
+            res.status(400).send({ message: `User ${id} not found` });
+        }
+    } catch (error) {
+        res.status(400).send({ message: `User ${id} not found` });
+    }
+}
+
+export const patchUser = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const user = await User.findByIdAndUpdate(userId, {avatar: req.file.path}, {new: true});
+        await user.save();
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+}
